de11e9799b0a914492b745dcd67cb4597d53b785
Tyuiu.SokolovaAA@outlook.com <nas.sok@mail.ru>
2024-12-23 08:19:17 +0500

    newAdd project files.

---
+++ SokolovaaaJournal/Data/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SokolovaaaJournal.Data
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Учетная запись обязательна")]
+        public int AccountId { get; set; }
+        [Required(ErrorMessage = "Дата обязательна")]
+        public DateTime Date { get; set; }
+        [Range(0.01, double.MaxValue, ErrorMessage = "Сумма должна быть положительным числом")]
+        public double Amount { get; set; }
+        [Required(ErrorMessage = "Тип обязателен")]
+        public string TransactionType { get; set; }
+        public string Description { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
+
+
