1d9f9d14c8f795e9bc07e9d53d8b92e62273e5bf
Tyuiu.SokolovaAA@outlook.com <nas.sok@mail.ru>
2024-12-23 08:19:17 +0500

    Add project files.

---
+++ SokolovaaaJournal/Data/Account.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SokolovaaaJournal.Data
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Имя обязательно")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Отдел обязателен")]
+        public string Department { get; set; }
+        [Range(0, double.MaxValue, ErrorMessage = "Баланс должен быть неотрицательным числом")]
+        public double Balance { get; set; }
+    }
+}
+
+
+
+
+
+
+
+
+
